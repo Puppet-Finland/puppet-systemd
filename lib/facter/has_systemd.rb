31aa5a2e8e84a46f024c0b4face500620300496b
Samuli Seppänen <samuli.seppanen@gmail.com>
2018-05-03 11:40:32 +0300

    Make Rubocop happy with fact indentation
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- lib/facter/has_systemd.rb
+++ lib/facter/has_systemd.rb
@@ -1,12 +1,11 @@
 Facter.add('has_systemd') do
+  if File.file?('/bin/systemctl') then
+    retval = true
+  else
+    retval = false
+  end
 
-    if File.file?('/bin/systemctl') then
-        retval = true
-    else
-        retval = false
-    end
-
-    setcode do
-        retval
-    end
+  setcode do
+    retval
+  end
 end
