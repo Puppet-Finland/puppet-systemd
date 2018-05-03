904bae8f8157e9f136defca06ea461d96aeecdf2
Samuli Seppänen <samuli.seppanen@gmail.com>
2018-05-03 11:56:02 +0300

    Further style fixes
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- lib/facter/has_systemd.rb
+++ lib/facter/has_systemd.rb
@@ -1,13 +1,10 @@
 Facter.add('has_systemd') do
-
   has_systemctl = File.file?('/bin/systemctl')
-
   if has_systemctl
     retval = true
   else
     retval = false
   end
-
   setcode do
     retval
   end
